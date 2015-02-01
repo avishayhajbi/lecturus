96439ed01653e246af721163bdf768e12e81f57d
avishay <avishayhajbi@gmail.com>
2015-02-01 12:16:06 +0200

    asdsad

--- index.js
+++ index.js
@@ -4,13 +4,13 @@
 var express = require('express');
 var mysql = require('mysql');
 var path = require('path');
-//var bodyParser  = require('body-parser');
+var bodyParser  = require('body-parser');
 var app = express();
 
 app.use(express.static(path.join(__dirname ,'views')));
-//app.use(bodyParser({limit: '50mb'}));
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
+app.use(bodyParser({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // middleware to check: 'Passport' -> for users managing
 
