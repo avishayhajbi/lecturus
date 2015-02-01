54dde3adf4b281e4d459afe4111042c256360ff0
avishay <avishayhajbi@gmail.com>
2015-02-01 11:47:39 +0200

    aaaa

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var path = require('path');
 //var bodyParser  = require('body-parser');
 var app = express();
 
-app.use(express.static(path.join(__dirname ,'views')));
+app.use(express.static(path.join(__dirname ,'/views')));
 //app.use(bodyParser({limit: '50mb'}));
 //app.use(bodyParser.urlencoded({extended: true}));
 //app.use(bodyParser.json());
