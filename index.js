52d6d09a7f7483fb711de82ce8871bc88f683b6f
avishay <avishayhajbi@gmail.com>
2015-02-02 23:14:24 +0200

    a

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ config ={
     Psql: "heroku pg:psql --app heroku-postgres-fa76e44a HEROKU_POSTGRESQL_SILVER"
 };
 
-pool = mysql.createPool(config);
+pool = mysql.createPool(config.databaseURL);
 
 app.listen(app.get('port'), function () {
     console.log('Server running...'+app.get('port'));
