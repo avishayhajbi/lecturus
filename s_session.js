ab78159130e38b2ab5c4f0b5ed867f5a18b6c645
avishay <avishayhajbi@gmail.com>
2015-04-16 11:33:10 +0300

    change upload

--- s_session.js
+++ s_session.js
@@ -1214,8 +1214,7 @@ router.post('/session/uploadAudio', function(request, response) {
         var file_name = this.openedFiles[0].name;
         console.log("file_name: " + file_name);
             
-        //var stream = cloudinary.uploader.upload_stream(function(result) 
-         cloudinary.uploader.upload(temp_path ,function(result)
+        var stream = cloudinary.uploader.upload_stream(function(result) 
         { 
           console.log(result)
            var r={};
@@ -1274,7 +1273,7 @@ router.post('/session/uploadAudio', function(request, response) {
           tags: [sessionId, 'lecturus']
         }      
       );
-      //var file_reader = fs.createReadStream(temp_path).pipe(stream);
+      var file_reader = fs.createReadStream(temp_path).pipe(stream);
     });
     
 });
